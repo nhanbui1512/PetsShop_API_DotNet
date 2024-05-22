c8d78d806708a123792ea022b99f18904bcf721c
nhanbui1512 <buithiennhan0345@gmail.com>
2024-05-22 22:12:22 +0700

    restore file migrations

---
+++ Migrations/20240513152408_v2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace petshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Gender",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Users");
+        }
+    }
+}
---
+++ Migrations/20240516091618_addFiedAvatar.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace petshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFiedAvatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "password");
+
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "Users",
+                newName: "gender");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "email");
+
+            migrationBuilder.AddColumn<string>(
+                name: "avatar",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "avatar",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "gender",
+                table: "Users",
+                newName: "Gender");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+        }
+    }
+}
