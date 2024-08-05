404951c4b5ea589aa99bf654069fe4cac0154a8b
Nhan Bui <buithiennhan0345@gmail.com>
2024-08-05 21:20:22 +0700

    update adminMiddleware

--- Middlewares/MiddlewareExtensions.cs
+++ Middlewares/MiddlewareExtensions.cs
@@ -2,17 +2,27 @@ using petshop.Middlewares;
 
 public static class MiddlewareExtensions
 {
-  public static IApplicationBuilder UseCustomMiddlewareForUser(this IApplicationBuilder app)
-  {
-    app.UseWhen(context => context.Request.Path.StartsWithSegments("/api/users") && context.Request.Method.Equals("GET", StringComparison.OrdinalIgnoreCase), appBuilder =>
+    public static IApplicationBuilder UseCustomMiddlewareForUser(this IApplicationBuilder app)
     {
-      appBuilder.UseMiddleware<AdminMiddleware>();
-    });
-    app.UseWhen(context => context.Request.Path.StartsWithSegments("/api/role") && context.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase), appBuilder =>
-   {
-     appBuilder.UseMiddleware<AdminMiddleware>();
-   });
 
-    return app;
-  }
+        var routesToProtect = new List<(string Path, string Method)>
+        {
+          ("/api/users", "GET"),
+          ("/api/role", "POST"),
+          ("/api/role", "GET"),
+          ("/api/role", "PUT"),
+          ("/api/orders/prepare", "PATCH"),
+        };
+        foreach (var route in routesToProtect)
+        {
+            app.UseWhen(context => context.Request.Path.StartsWithSegments(route.Path) &&
+                                   context.Request.Method.Equals(route.Method, StringComparison.OrdinalIgnoreCase),
+            appBuilder =>
+            {
+                appBuilder.UseMiddleware<AdminMiddleware>();
+            });
+        }
+
+        return app;
+    }
 }
--- Models/Bill.cs
+++ Models/Bill.cs
@@ -7,19 +7,19 @@ using System.Threading.Tasks;
 
 namespace petshop.Models
 {
-    [Table("payments")]
-    public class Bill
-    {
-        [Column("id")]
-        [Key]
-        public int Id { get; set; }
-        [Column("total")]
-        public decimal Total { get; set; }
-        [Column("created_at")]
-        public DateTime PayAt { get; set; } = DateTime.Now;
-        [Column("updated_at")]
-        public DateTime UpdatedAt { get; set; }
-        public Order? Order { get; set; }
-        public int OrderId { get; set; }
-    }
+  [Table("payments")]
+  public class Bill
+  {
+    [Column("id")]
+    [Key]
+    public int Id { get; set; }
+    [Column("total")]
+    public decimal Total { get; set; }
+    [Column("created_at")]
+    public DateTime PayAt { get; set; } = DateTime.Now;
+    [Column("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+    public Order? Order { get; set; }
+    public int OrderId { get; set; }
+  }
 }
